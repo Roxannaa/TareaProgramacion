9dfbd456b872f953e1276c5ee26ad10a01608d77
Johan <Johanmanuel13579@gmail.com>
2020-02-26 19:42:53 -0400

    se creó controlador de contenidos y la vista del Index

---
+++ TareaProg4/Controllers/ContenidoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TareaProg4.Controllers
+{
+    public class ContenidoController : Controller
+    {
+        // GET: Contenido
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
