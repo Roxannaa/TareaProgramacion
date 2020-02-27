d93b7fbe592dff4e36b5a4c85135a8ca6dd93873
Roxanna <roxanna.alb.c@gmail.com>
2020-02-27 00:19:04 -0600

    Llame los datos de la clase campo y los archivos del formulario al controlador. Asimismo llame con @Model estos datos en la vista Datos para que pudieran mostrarse. El boton enviar lleva directamente a esta pagina luego de cumplir satisfactoriamente el formulario. Los documentos cargados por el usuario ya se pueden descargar a la carpeta img y mostrarse en la vista Datos con @ViewBag. Aun hay problemas mostrando el documento pdf en esta vista.

--- TareaProg4/Controllers/FormController.cs
+++ TareaProg4/Controllers/FormController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TareaProg4.Models;
 
 namespace TareaProg4.Controllers
 {
@@ -13,11 +14,29 @@ namespace TareaProg4.Controllers
         {
             return View();
         }
+    
 
-        public ActionResult Datos()
+    [HttpPost]
+    public ActionResult Datos(Campos formu, HttpPostedFileBase imagen, HttpPostedFileBase cv)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
-        }
+            string nombreFoto = imagen.FileName;
+            imagen.SaveAs(Server.MapPath("/img/" + nombreFoto));
 
+            string curriculum = cv.FileName;
+            cv.SaveAs(Server.MapPath("/img/" + curriculum));
+
+            ViewBag.foto = nombreFoto;
+            ViewBag.cv = curriculum;
+            return View(formu);
+        }
+        else
+        {
+            return View("Formulario");
+        }
     }
+
+  }
+
 }
