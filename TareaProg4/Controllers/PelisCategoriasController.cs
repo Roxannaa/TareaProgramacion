49007055a6abe939444842dfcb4e866a0a542f36
Johan <Johanmanuel13579@gmail.com>
2020-02-26 20:21:03 -0400

    se creó un controlador para las peliculas para dividirlas por categoría, además de que se modificó la ruta para ingresar correctamente a las vistas de las peliculas

---
+++ TareaProg4/Controllers/PelisCategoriasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TareaProg4.Controllers
+{
+    public class PelisCategoriasController : Controller
+    {
+        // GET: PelisCategorias
+        public ActionResult Accion()
+        {
+            return View();
+        }
+        public ActionResult Aventura()
+        {
+            return View();
+        }
+        public ActionResult Drama()
+        {
+            return View();
+        }
+        public ActionResult Infantil()
+        {
+            return View();
+        }
+        public ActionResult Documental()
+        {
+            return View();
+        }
+    }
+}
